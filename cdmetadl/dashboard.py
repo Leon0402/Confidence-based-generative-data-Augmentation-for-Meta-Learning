7045001c4bc81c15dbbc6c03829445d11cee37d6
Leon De Andrade <leondeandrade@mailo.com>
2023-12-11 10:06:31 +0100

    Add graph per feature

--- cdmetadl/dashboard.py
+++ cdmetadl/dashboard.py
@@ -4,7 +4,6 @@ import dash
 import dash_mantine_components as dmc
 import pandas as pd
 import plotly.figure_factory as ff
-import plotly.graph_objs as go
 import plotly.express as px
 
 all_metrics = ['Normalized Accuracy', 'Accuracy', 'Macro F1 Score', 'Macro Precision', 'Macro Recall']
@@ -20,6 +19,19 @@ def sort_metrics(selected_metrics):
 
 def get_data(models):
     return pd.concat([dfs[model_name] for model_name in models])
+
+
+def convert_df_to_dash(df):
+    """
+    Converts a pandas data frame to a format accepted by dash
+    Returns columns and data in the format dash requires
+    """
+    ids = ["".join([col for col in multi_col if col]) for multi_col in list(df.columns)]
+
+    cols = [{"name": list(col), "id": id_} for col, id_ in zip(list(df.columns), ids)]
+    data = [{k: v for k, v in zip(ids, row)} for row in df.values]
+
+    return cols, data
 
 
 app = dash.Dash(__name__)
@@ -60,9 +72,41 @@ app.layout = dmc.Container([
                     dash.html.Div(
                         id='histogram-container', style={
                             "display": "grid",
-                            "gridTemplateColumns": "repeat(auto-fit, minmax(800px, 1fr))",
+                            "gridTemplateColumns": "repeat(auto-fit, minmax(600px, 1fr))",
                         }
-                    )
+                    ),
+                    dmc.Title("Scores per Feature", order=1),
+                    dmc.SegmentedControl(
+                        id="feature-selector",
+                        value="dataset",
+                        data=[
+                            {
+                                "value": "dataset",
+                                "label": "Dataset"
+                            },
+                            {
+                                "value": "way",
+                                "label": "Way"
+                            },
+                            {
+                                "value": "shot",
+                                "label": "Shot"
+                            },
+                        ],
+                        mt=10,
+                    ),
+                    dash.dash_table.DataTable(
+                        id='scores_per_dataset_table',
+                        style_table={'overflowX': 'auto'},
+                        style_cell={'textAlign': 'center'},
+                        merge_duplicate_headers=True,
+                    ),
+                    dash.html.Div(
+                        id='box-plot-container', style={
+                            "display": "grid",
+                            "gridTemplateColumns": "repeat(auto-fit, minmax(600px, 1fr))",
+                        }
+                    ),
                 ]
             ),
         ]
@@ -88,7 +132,6 @@ def average_table(metrics, models):
 )
 def overall_frequence_histogram(metrics, models):
     df = get_data(models)
-
     histogram_figures = []
     for metric in sort_metrics(metrics):
         hist_data = [group for _, group in df.groupby('Model')[metric]]
@@ -101,6 +144,38 @@ def overall_frequence_histogram(metrics, models):
         histogram_figures.append(dash.dcc.Graph(figure=fig))
 
     return histogram_figures
+
+
+@app.callback([dash.Output('scores_per_dataset_table', 'columns'),
+               dash.Output('scores_per_dataset_table', 'data')], [
+                   dash.Input('feature-selector', 'value'),
+                   dash.Input('metric-selector', 'value'),
+                   dash.Input('model-selector', 'value')
+               ])
+def scores_per_dataset(feature, metrics, models):
+    df = get_data(models)
+    averages = df.pivot_table(index=feature, columns='Model', values=metrics).reset_index()
+    return convert_df_to_dash(averages)
+
+
+@app.callback(
+    dash.Output('box-plot-container', 'children'), [
+        dash.Input('feature-selector', 'value'),
+        dash.Input('metric-selector', 'value'),
+        dash.Input('model-selector', 'value')
+    ]
+)
+def box_plots(feature, metrics, models):
+    df = get_data(models)
+
+    return [
+        dash.dcc.Graph(
+            figure=px.box(
+                df[["Model", feature, metric]], x=feature, y=metric, color="Model",
+                title=f"Box Plot of {metric} by Model and {feature}"
+            )
+        ) for metric in sort_metrics(metrics)
+    ]
 
 
 if __name__ == '__main__':
