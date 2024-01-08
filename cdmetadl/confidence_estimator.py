81bd5d8be791876676cceca3fbb2913cb5bc61e6
vilab23 vilab23 <vilab23@VISINF.LOCAL@lab05.visinf.local>
2024-01-08 13:40:12 +0100

    Added comments to all functions, fixed naming and small bug in augmentation

--- cdmetadl/confidence_estimator.py
+++ cdmetadl/confidence_estimator.py
@@ -6,17 +6,32 @@ import cdmetadl.helpers.general_helpers
 import cdmetadl.helpers.scoring_helpers
 import cdmetadl.dataset.split
 
+# TODO: makes this more modular for other methods
 
-
-# should be in its own file
 def ref_set_confidence_scores(conf_support_set: tuple[torch.Tensor, torch.Tensor, torch.Tensor], conf_query_set: tuple[torch.Tensor, torch.Tensor, torch.Tensor], learner: MyLearner) -> dict: 
-    # for pseudo augmentation
-    # returns dict with confidence scores for each class found in this task, calculated as 0 if prediciton is false or a mean of the softmaxes over all shots if prediction is true
+    """
+    Calculates confidence score for every class in task by pretraining/finetuning on validation/conf_support_set and prediction on validation query_set. Returns the mean of confidence scores per 
+    class over every where a particular class was the correct predction. If class C is correctly classified, the CS will be the output of the softmax, if it is wrongly classified it will be 0. 
+
+    Args:
+        conf_support_set: (tuple[torch.Tensor, torch.Tensor, torch.Tensor]): Support set used for pretraining the model on.
+        conf_query_set: (tuple[torch.Tensor, torch.Tensor, torch.Tensor]): Query set for prediction after pretraining and estimation of confidence scores through softmax. 
+        learner: (MyLeaner) pretrained model instatiated for meta-testing. 
+
+    Returns:
+        dict: dictionary with key being class and value the confidence estimate for that class.
+    """
     conf_scores = dict()
     learner.fit(support_set)
+    # TODO: check for exact output of predictor.predict
     predictions = predictor.predict(query_set[0])
     # need softmax and label of predicted item
     print("predictions: ", predictions)
+
+    # check if max(softmax) is of class same as ground truth -> add softmax to dict
+    # otherwise add 0 to dict, keeping running average
+    #for prediciton in predictions: 
+    #    if 
 
 
     return conf_scores
